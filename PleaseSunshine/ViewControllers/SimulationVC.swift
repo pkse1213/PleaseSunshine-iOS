16423cbb14a81b7724aa313c7a6e8c3c804566c1
박세은 <pkse1213@naver.com>
2018-11-23 18:51:30 +0900

    비용 탭 뷰 완료

--- PleaseSunshine/ViewControllers/SimulationVC.swift
+++ PleaseSunshine/ViewControllers/SimulationVC.swift
@@ -11,8 +11,15 @@ import UIKit
 class SimulationVC: UIViewController {
 
     
+    @IBOutlet weak var lookBtn: UIButton!
+    @IBOutlet weak var yearLbl: UILabel!
+    @IBOutlet weak var priceLbl: UILabel!
+    @IBOutlet weak var infoLbl: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        yearLbl.adjustsFontSizeToFitWidth = true
+        priceLbl.adjustsFontSizeToFitWidth = true
+        infoLbl.adjustsFontSizeToFitWidth = true
         setLogoInNaviBar()
         print("simulateor")
     }
