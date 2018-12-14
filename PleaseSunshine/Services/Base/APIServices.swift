5d192586428cfecec0cb7830d9e2e19046e32481
박세은 <pkse1213@naver.com>
2018-12-14 16:40:51 +0900

    url 변경

--- PleaseSunshine/Services/Base/APIServices.swift
+++ PleaseSunshine/Services/Base/APIServices.swift
@@ -13,6 +13,6 @@ protocol APIService {
 
 extension APIService {
     static func url(_ path: String) -> String {
-        return "http://52.231.66.80:8888" + path
+        return "http://13.125.253.169:8888" + path
     }
 }
